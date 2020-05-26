f8b7f08f664726d447a2a6d83082cad210b90a4b
Takeru Saito <takelushi@gmail.com>
2020-05-26 14:51:22 +0900

    deploy

---
+++ precache-manifest.8c38eb09d44df900e8901fdcc8748e26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe32fca26cae1732d19",
+    "url": "/vue-pwa/css/chunk-vendors.2662c5ff.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vue-pwa/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3b161577dfbb407311a247060db521f7",
+    "url": "/vue-pwa/index.html"
+  },
+  {
+    "revision": "d98138a15fa8cd8013aa",
+    "url": "/vue-pwa/js/about.a01894cb.js"
+  },
+  {
+    "revision": "48d5dff9a81d2c6fe2aa",
+    "url": "/vue-pwa/js/app.50e9b510.js"
+  },
+  {
+    "revision": "bbe32fca26cae1732d19",
+    "url": "/vue-pwa/js/chunk-vendors.930a49c1.js"
+  },
+  {
+    "revision": "a7b985238384dc336f0a16d577f68212",
+    "url": "/vue-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa/robots.txt"
+  }
+]);
